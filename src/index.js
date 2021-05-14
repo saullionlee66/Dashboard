aa4a61701b904b5db1b804ecc1f7f76976079592
Saul <saullionlee@hotmail.com>
2021-05-13 21:43:39 -0400

    commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Demo from "./demo";
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(<Demo />, document.querySelector("#root"));
